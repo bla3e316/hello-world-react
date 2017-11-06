fcfa6ec22d8aff397b2ad8e32c1a7cfa9f69a677
Aleks <bla3e316@gmail.com>
2017-11-06 14:42:54 +0200

    Initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+      console.log('Hello from App.jsx');
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>);
+  }
+}
